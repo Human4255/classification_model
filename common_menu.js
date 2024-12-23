08d1953d1451254d587afe5a82401a2dfd2cec1d
Human4255 <sunga4255@gmail.com>
2024-12-23 14:46:26 +0900

    race게임

--- common_menu.js
+++ common_menu.js
@@ -17,7 +17,7 @@ menu1.tips = "구글,네이버 이미지 크롤링 및 컨볼루션 적용한";
 menu2 = new Menu("TEST MENU");
 menu2.url = "https://human4255.github.io/classification_model/";
 menu2.tips = "테스트용";
-
+menu_sets.push(menu0)
 menu_sets.push(menu1)
 menu_sets.push(menu2)
 //메뉴 생성기 종료 $================================
